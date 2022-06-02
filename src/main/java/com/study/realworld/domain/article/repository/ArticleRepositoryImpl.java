db7379b9ec1580ca9cdf6409a19c7e57f7aad825
Tianea2160 <rhlehfndvkd7557@daum.net>
2022-06-02 22:52:58 +0900

    refactor feed service : refactor for reducing query feed article

--- src/main/java/com/study/realworld/domain/article/repository/ArticleRepositoryImpl.java
+++ src/main/java/com/study/realworld/domain/article/repository/ArticleRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.study.realworld.domain.article.repository;
 
 import com.study.realworld.domain.article.Article;
+import com.study.realworld.domain.member.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -39,5 +41,12 @@ public class ArticleRepositoryImpl implements ArticleRepoCustom {
         } catch (NoResultException e) {
             return Optional.empty();
         }
+    }
+
+    // testing
+    public List<Member> findFeed(Long loginMemberId){
+        return em.createQuery("select m from Member m join fetch m.followers where m.id = :id", Member.class)
+                .setParameter("id", loginMemberId)
+                .getResultList();
     }
 }
